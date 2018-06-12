39419cb346b5859f2fd9c09df3bdfde9aa9b3468
Rafael Alves <rafael.alvessilva1995@gmail.com>
2018-06-12 19:44:20 -0300

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <img class="gramado" src="https://cartolafc.globo.com/dist/3.5.7/img/campinho-sem-borda.svg">
+  </q-page>
+</template>
+
+<style>
+.gramado {
+  width: 90%;
+}
+</style>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+    }
+  },
+  components: {
+    draggable
+  }
+}
+</script>
